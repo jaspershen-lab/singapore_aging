b32d20e7de1d1462cc38deaf0e90e6d9750f25f8
jaspershen <shenxt1990@outlook.com>
2024-01-16 10:32:46 -0800

    update

--- 1-code/4-population_age_percentage.R
+++ 1-code/4-population_age_percentage.R
@@ -5,119 +5,151 @@ library(tidyverse)
 setwd(r4projects::get_project_wd())
 
 # Read data
+###data1 is historical data, and data 2 is predicted data
 data1 <- readr::read_csv("2-data/age_structure_data_historical.csv")
 data2 <- readr::read_csv("2-data/age_structure_data_predicted.csv")
 
+data1$`Reference area`[data1$`Reference area` == "China (People’s Republic of)"] <- "China"
 
 data1 <-
 data1  %>% 
-dplyr::select(Country, Sex, Age, Time, Value)  %>% 
-dplyr::filter(Age %in% c("0 to 4", "5 to 9", "10 to 14", "15 to 19", "20 to 24", "25 to 29",
-                                     "30 to 34", "35 to 39", "40 to 44", "45 to 49",
-                                     "50 to 54", "55 to 59", "60 to 64", "65 to 69",
-                                     "70 to 74", "75 to 79", "80 to 84", "85 and over")) %>%
-                                     dplyr::filter(Sex == "Total")  %>% 
-dplyr::filter(Country %in% c("Singapore", "China (People's Republic of)", "Japan", "Korea", "United States", "Germany"))
+dplyr::rename(Country = "Reference area",
+Time_horizon = "Time horizon",
+Time = "TIME_PERIOD",
+Value = OBS_VALUE)  %>% 
+dplyr::select(Country, Sex, Time_horizon, Age, Time, Value) %>% 
+    dplyr::filter(Age %in% c("4 years or less", 
+    "From 5 to 9 years",
+    "From 10 to 14 years",
+    "From 15 to 19 years",
+    "From 20 to 24 years",
+    "From 25 to 29 years",
+    "From 30 to 24 years",
+    "From 35 to 39 years",
+    "From 40 to 44 years",
+    "From 45 to 49 years",
+    "From 50 to 54 years",
+    "From 55 to 59 years",
+    "From 60 to 64 years",
+    "From 65 to 69 years",
+    "From 70 to 74 years",
+    "From 75 to 79 years",
+    "From 80 to 84 years",
+    "85 years or over")) %>%
+    dplyr::filter(Sex == "Total") %>%
+        dplyr::mutate(Age_range = case_when(Age %in% c("4 years or less", "From 5 to 9 years", 
+    "From 10 to 14 years", "From 15 to 19 years") ~ "0-19",
+    Age %in% c("From 20 to 24 years", "From 25 to 29 years", "From 30 to 34 years", "From 35 to 39 years", "From 40 to 44 years",
+    "From 45 to 49 years", "From 50 to 54 years", "From 55 to 59 years", "From 60 to 64 years") ~ "20-64",
+    Age %in% c("From 65 to 69 years", "From 70 to 74 years", "From 75 to 79 years", "From 80 to 84 years", "85 years or over") ~ "Over 65")) %>%
+    dplyr::filter(Country %in% c("Singapore", "China", "Japan", "Korea", "United States", "Germany"))
 
 data1 <-
+data1 %>%
+    dplyr::group_by(Country, Time) %>%
+    dplyr::summarize(rate = Value / sum(Value) * 100,
+                                     Age_range = Age_range) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(Country, Age_range, Time) %>%
+    dplyr::summarize(rate = sum(rate))
+
+# This code takes the 'data1' dataframe and adds a new column 'Age_range' as a factor variable.
+# The levels of the 'Age_range' variable are set to "Over 65", "20-64", and "0-19".
+# It then creates a bar plot using ggplot, where the x-axis represents 'Time' and the y-axis represents 'rate'.
+# The bars are filled based on the 'Age_range' variable.
+# The plot is displayed in a grid of facets, with each facet representing a different 'Country'.
+# The grid has 3 columns.
+data1 %>%
+    dplyr::mutate(Age_range = factor(Age_range, levels = c("Over 65", "20-64", "0-19"))) %>%
+    ggplot(aes(Time, rate)) +
+    geom_bar(stat = "identity", aes(fill = Age_range)) +
+    theme_bw() +
+    facet_wrap(~Country, ncol = 3)
+
+# This code filters the 'data1' dataframe to include only the rows where the 'Age_range' column is "Over 65".
+# It then creates a line plot using ggplot, where the x-axis represents the 'Time' column and the y-axis represents the 'rate' column.
+# Each data point is colored based on the 'Country' column, and lines are drawn to connect the data points for each country.
+
 data1  %>% 
-dplyr::group_by(Country, Time)  %>% 
-dplyr::summarize(rate = Value/sum(Value) * 100,
-Age = Age)
-
-###for data1, I want to dra
-data1  %>% 
-dplyr::filter(Country == "Singapore")  %>%
-ggplot(aes())
+    dplyr::filter(Age_range == "Over 65")  %>% 
+    ggplot(aes(Time, rate)) +
+    geom_point(aes(color = Country)) +
+    geom_line(aes(group = Country, color = Country))
 
 
-total <-
-data2  %>% 
-dplyr::filter(Country == "Singapore")  %>% 
-dplyr::filter(Age == "Total",
-Sex == "Total")  %>%
-dplyr::select(Time, Age, Value)  %>% 
-dplyr::arrange(Time)
 
-over60 <- 
-data2  %>% 
-dplyr::filter(Country == "Singapore")  %>%
-dplyr::filter(AGE == "65_OVER",
-Sex == "Total")  %>% 
-dplyr::select(Time, Age, Value)  %>% 
-dplyr::arrange(Time)
+data2$`Reference area`[data2$`Reference area` == "China (People’s Republic of)"] <- "China"
 
 data2 <-
-total  %>% 
-dplyr::mutate(rate = over60$Value / total$Value * 100)
+data2  %>% 
+dplyr::rename(Country = "Reference area",
+Time_horizon = "Time horizon",
+Time = "TIME_PERIOD",
+Value = OBS_VALUE)  %>% 
+dplyr::select(Country, Sex, Time_horizon, Age, Time, Value) %>% 
+    dplyr::filter(Age %in% c("4 years or less", 
+    "From 5 to 9 years",
+    "From 10 to 14 years",
+    "From 15 to 19 years",
+    "From 20 to 24 years",
+    "From 25 to 29 years",
+    "From 30 to 24 years",
+    "From 35 to 39 years",
+    "From 40 to 44 years",
+    "From 45 to 49 years",
+    "From 50 to 54 years",
+    "From 55 to 59 years",
+    "From 60 to 64 years",
+    "From 65 to 69 years",
+    "From 70 to 74 years",
+    "From 75 to 79 years",
+    "From 80 to 84 years",
+    "85 years or over")) %>%
+    dplyr::filter(Sex == "Total") %>%
+        dplyr::mutate(Age_range = case_when(Age %in% c("4 years or less", "From 5 to 9 years", 
+    "From 10 to 14 years", "From 15 to 19 years") ~ "0-19",
+    Age %in% c("From 20 to 24 years", "From 25 to 29 years", "From 30 to 34 years", "From 35 to 39 years", "From 40 to 44 years",
+    "From 45 to 49 years", "From 50 to 54 years", "From 55 to 59 years", "From 60 to 64 years") ~ "20-64",
+    Age %in% c("From 65 to 69 years", "From 70 to 74 years", "From 75 to 79 years", "From 80 to 84 years", "85 years or over") ~ "Over 65")) %>%
+    dplyr::filter(Country %in% c("Singapore", "China", "Japan", "Korea", "United States", "Germany"))
 
-# data2 <-
-# data2  %>% 
-# dplyr::filter(Country == "Singapore")  %>% 
-# dplyr::filter(Age != "Total")  %>% 
-# dplyr::filter(AGE %in% c("0_4", "05_9", "10_14", "15_19", "20_24",
-#          "25_29", "30_34", "35_39", "40_44", "45_49",
-#          "50_54", "55_59", "60_64", "65_69",
-#          "70_74", "75_79", "80_84", "85_OVER"))  %>% 
-#          dplyr::group_by(AGE)  %>% 
-#          dplyr::summarize(AGE = AGE,
-#          TIME = TIME,
-#          Value = sum(Value))
+data2 <-
+data2 %>%
+    dplyr::group_by(Country, Time) %>%
+    dplyr::summarize(rate = Value / sum(Value) * 100,
+                                     Age_range = Age_range) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(Country, Age_range, Time) %>%
+    dplyr::summarize(rate = sum(rate))
 
-
-dir.create("3-data1_analysis/4-population_age_percentage", showWarnings = FALSE)
-
-setwd("3-data1_analysis/3-singapore_population_age_percentage")
-
-data1$`Data Series`
-
-data1 <-
-data1  %>% 
-dplyr::select(-"Data Series")  %>% 
-head(1)  %>% 
-t()  %>% 
-as.data.frame()  %>% 
-tibble::rownames_to_column(var = "year" ) 
-
-colnames(data1)[2] <- "rate"
-
-data1 <-
-data1  %>% 
-dplyr::filter(year != "1957")
-
-data1$rate <- as.numeric(data1$rate)
-
-data1$year
-
-
-data2$Time <-
-as.character(data2$Time)
-
-data1  %>% 
-dplyr::full_join(data2[,c("Time", "rate")],
-by = c("year" = "Time"))  %>% 
-dplyr::filter(!is.na(rate.x) & !is.na(rate.y))  %>% 
-ggplot(aes(rate.x, rate.y)) +
-geom_point() +
-geom_abline(intercept = 0, slope = 1)
-
-
-plot <-
-data1 %>% 
-    ggplot(aes(year, rate)) +
-    geom_point() +
-    geom_line(aes(group = 1)) +
-    # geom_hline(yintercept = 65,
-    # color = "red") +
+# This code takes the 'data2' dataframe and adds a new column 'Age_range' as a factor variable.
+# The levels of the 'Age_range' variable are set to "Over 65", "20-64", and "0-19".
+# It then creates a bar plot using ggplot, where the x-axis represents 'Time' and the y-axis represents 'rate'.
+# The bars are filled based on the 'Age_range' variable.
+# The plot is displayed in a grid of facets, with each facet representing a different 'Country'.
+# The grid has 3 columns.
+data2 %>%
+    dplyr::mutate(Age_range = factor(Age_range, levels = c("Over 65", "20-64", "0-19"))) %>%
+    ggplot(aes(Time, rate)) +
+    geom_bar(stat = "identity", aes(fill = Age_range)) +
     theme_bw() +
-    theme(panel.grid.minor = element_blank(),
-    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-    labs(x = "Year",
-    y = "Percentage of population ages 65 or older")
-    # scale_y_continuous(limits = c(0, 6))
+    facet_wrap(~Country, ncol = 3)
 
+# This code filters the 'data2' dataframe to include only the rows where the 'Age_range' column is "Over 65".
+# It then creates a line plot using ggplot, where the x-axis represents the 'Time' column and the y-axis represents the 'rate' column.
+# Each data point is colored based on the 'Country' column, and lines are drawn to connect the data points for each country.
 
-plot
+data2  %>% 
+    dplyr::filter(Age_range == "Over 65")  %>% 
+    ggplot(aes(Time, rate)) +
+    geom_point(aes(color = Country)) +
+    geom_line(aes(group = Country, color = Country))
 
-ggsave(plot, filename = "singapore_population_age_percentage.pdf", 
-width = 10, height = 6, units = "in")
+rbind(data1, data2)  %>% 
+dplyr::filter(Age_range == "Over 65")  %>% 
+dplyr::filter(Country == "Singapore")  %>%
+ggplot(aes(Time, rate)) +
+geom_point(aes(color = Country)) +
+geom_line(aes(group = Country,
+color = Country)) +
+theme_bw()
